e6626df9cec177fecf6ac5c662b032c9d87e5fa9
Nileesha-ap <nileesha.pawaskar@invenger.com>
2021-06-08 11:05:33 +0530

    OTP Screen-08 June,2021

--- lib/Screens/otpScreen.dart
+++ lib/Screens/otpScreen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:medicalemergency/Screens/mainScreen.dart';
 import 'package:pinput/pin_put/pin_put.dart';
 
 class OTPScreen extends StatefulWidget {
@@ -16,10 +18,10 @@ class _OTPScreenState extends State<OTPScreen> {
   final TextEditingController _pinPutController = TextEditingController();
   final FocusNode _pinPutFocusNode = FocusNode();
   final BoxDecoration pinPutDecoration = BoxDecoration(
-    color: const Color.fromRGBO(43, 46, 66, 1),
+    color: const Color.fromRGBO(153, 0, 0, 1),
     borderRadius: BorderRadius.circular(10.0),
     border: Border.all(
-      color: const Color.fromRGBO(126, 203, 224, 1),
+      color: const Color.fromRGBO(153, 0, 0, 1),
     ),
   );
 
@@ -56,31 +58,67 @@ class _OTPScreenState extends State<OTPScreen> {
             selectedFieldDecoration: pinPutDecoration,
             followingFieldDecoration: pinPutDecoration,
             pinAnimationType: PinAnimationType.fade,
-            // onSubmit: (pin) async {
-            // try {
-            // await FirebaseAuth.instance
-            //     .signInWithCredential(PhoneAuthProvider.credential(
-            // verificationId: _verificationCode, smsCode: pin))
-            //     .then((value) async {
-            // if (value.user != null) {
-            // Navigator.pushAndRemoveUntil(
-            // context,
-            // MaterialPageRoute(builder: (context) => Home()),
-            // (route) => false);
-            // }
-            // });
-            // } catch (e) {
-            // FocusScope.of(context).unfocus();
-            //  _scaffoldkey.currentState
-            //
-            //    }
-            //.showSnackBar(SnackBar(content: Text('invalid OTP')));
-            // }
-
+            onSubmit: (pin) async {
+              try {
+                await FirebaseAuth.instance
+                    .signInWithCredential(PhoneAuthProvider.credential(
+                    verificationId: _verificationCode, smsCode: pin))
+                    .then((value) async {
+                  if (value.user != null) {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                            (route) => false);
+                  }
+                });
+              } catch (e) {
+                FocusScope.of(context).unfocus();
+                _scaffoldkey.currentState
+                    .showSnackBar(SnackBar(content: Text('invalid OTP')));
+              }
+            }
           ),
           )
         ],
       ),
     );
+  }
+  _verifyPhone() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: '+91${widget.phone}',
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await FirebaseAuth.instance
+              .signInWithCredential(credential)
+              .then((value) async {
+            if (value.user != null) {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                      (route) => false);
+            }
+          });
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print(e.message);
+        },
+
+
+        codeSent: (String verficationID, int resendToken) {
+          setState(() {
+            _verificationCode = verficationID;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationID) {
+          setState(() {
+            _verificationCode = verificationID;
+          });
+        },
+        timeout: Duration(seconds: 120));
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _verifyPhone();
   }
 }
